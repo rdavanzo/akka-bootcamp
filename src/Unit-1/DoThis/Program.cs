61e7ed99412c0d66d671ef3c7c32852f063d269a
Ryan Davanzo <ryan.devoted@gmail.com>
2016-07-05 09:19:45 -0700

    Completing lesson 3

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -10,10 +10,18 @@ namespace WinTail
         static void Main(string[] args)
         {
             MyActorSystem = ActorSystem.Create("MyActorSystem");
-            var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
-            var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
-            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
+            var consoleWriterProps = Props.Create<ConsoleWriterActor>();
+            var consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriteActor");
+
+            var validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            var validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+
+            var consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+            var consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
+
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+            
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.AwaitTermination();
         }
